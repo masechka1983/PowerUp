4598595ec8f53ac3a39c865ef23d1107f7e42b27
Yevgeniya <masechka1983@gmail.com>
2014-02-06 09:16:18 +0100

    Changed action

--- resultat.php
+++ resultat.php
@@ -36,7 +36,7 @@
     <form class="contact" method="post" action="index.php">
 
       <div id="competimage">
-      	<form action="upload_file.php" method="post" enctype="multipart/form-data">
+      	<form action="resultat.php" method="post" enctype="multipart/form-data">
 			<label for="file">Filename:</label>
 			<input type="file" name="file" id="file"><br>
 			<input type="submit" name="submit" value="Submit">
