a750b35353b29c521daab823cb657c2bdb634219
Yevgeniya <masechka1983@gmail.com>
2014-02-06 13:11:53 +0100

    Added confirm.php

---
+++ confirm.php
@@ -0,0 +1,26 @@
+<?php
+    error_reporting(E_ALL);
+
+	$powerup = $_POST['powerup'];
+	$navn = $_POST['navn'];
+	$email = $_POST['email'];
+	$instrukser = $_POST['instrukser'];
+
+//Opretter DB-forbindelse
+	$dblink = mysqli_connect("localhost","mod7af1489","2faumoj9","mod7af1489") or die ("Fejl: Kan ikke etablere forbindelse til databasen");
+	mysqli_set_charset($dblink, "utf8");
+
+//Først SQL-forespørslen- hent alt fra din tabel
+	$guery = "SELECT * FROM goodvertizingkonkurrencedeltagere";
+
+//Udfør dernæst overstående SQL-sætning
+	$result = mysqli_query($dblink, $query) or die ("Førespørgslen kunne ikke udføres: " . mysqli_error($dblink));
+
+	while($row = mysqli_fetch_assos($result)){
+		echo "<pre>";
+		print_r($row);
+		echo "<hr>";
+		echo "</pre>";
+	}
+?>
+
