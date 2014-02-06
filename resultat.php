52ae7ec80efe595165bd8b8112ab6fac2659c961
Yevgeniya <masechka1983@gmail.com>
2014-02-06 11:24:48 +0100

    Deleted image form and code to that in resultant.php

--- resultat.php
+++ resultat.php
@@ -5,7 +5,6 @@
 	$navn = $_POST['navn'];
 	$email = $_POST['email'];
 	$instrukser = $_POST['instrukser'];
-    $image = $_FILES['imgfile']['name'];
 ?>
 
 <!DOCTYPE html>
@@ -30,37 +29,7 @@
     <form class="contact" method="post" action="index.php">
 
       <div id="competimage">
-      	<!--?php
-		if(isset($_REQUEST['submit']))
-		{
-		  $filename=  $_FILES["imgfile"]["name"];
-		  if ((($_FILES["imgfile"]["type"] == "image/gif")|| ($_FILES["imgfile"]["type"] == "image/jpeg") || ($_FILES["imgfile"]["type"] == "image/png")  || ($_FILES["imgfile"]["type"] == "image/pjpeg")) && ($_FILES["imgfile"]["size"] < 200000))
-		  {
-		    if(file_exists($_FILES["imgfile"]["name"]))
-		    {
-		      echo "File name exists.";
-		    }
-		    else
-		    {
-		      move_uploaded_file($_FILES["imgfile"]["tmp_name"],"uploads/$filename");
-		      echo "Upload Successful . <a href='uploads/$filename'>Click here</a> to view the uploaded image";
-		    }
-		  }
-		  else
-		  {
-		    echo "invalid file.";
-		  }
-		}
-		else
-		{
-		?-->
-		<form method="post" enctype="multipart/form-data">
-			<input type="file" name="imgfile"><br>
-			<input type="submit" name="submit" value="upload">
-		</form>
-		<!--?php
-		}
-		?--> 
+        <input type="file">
       </div>
 
       <div id="compeform">
