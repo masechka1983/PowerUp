e57be4a945e5799b6c0994b45674d169021871b6
Yevgeniya <masechka1983@gmail.com>
2014-02-07 15:21:07 +0100

    Added winners and in style.css added reset part

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
-  $aar = date['Y'];
-  $maaned = date['m'];
-  $dag = date['d'];
+  $aar = date('Y');
+  $maaned = date('m');
+  $dag = date('d');
 
   $slutaar = '2014';
   $slutmaaned = '02';
@@ -27,7 +27,7 @@
   </section>
 
   <?php 
-   if($konkurrencestatus = true){
+   if($konkurrencestatus){
     ?>
     <section class="competition">
       <h1 class="title">Opskriv dit øvelse</h1>
@@ -61,9 +61,31 @@
     }      
      else{
       echo "<h1>Konkurrencen er slut!</h1>";
+
+      echo "<h2>De heldige vindere er:</h2>";
+
+      //Opretter DB-forbindelse
+        $dblink = mysqli_connect("localhost","mod7af1489","2faumoj9","mod7af1489") or die ("Fejl: Kan ikke etablere forbindelse til databasen");
+        mysqli_set_charset($dblink, "utf8");
+
+      //Først SQL-forespørslen- hent alt fra din tabel
+      $query = "SELECT * FROM goodvertizingkonkurrencedeltagere WHERE id in (1, 2, 3, 4, 5)";  
+
+      $result = mysqli_query($dblink, $query) or die ("Førespørgslen kunne ikke udføres: " . mysqli_error($dblink));
+
+      echo "<section>";
+      while($row = mysqli_fetch_assoc($result)){
+        echo "<article>";
+        echo "<hr>";
+        echo "<p>{$row['brugersnavn']}</p>"; 
+        echo "</article>";
+      }
+      echo "</section>";
+
+      echo "<p>Vi kontakter Jer via email";
      }
   
-  
+    ?>
 
   
   
